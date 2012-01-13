c1c36980e5e070dee218d00fed2111932b8fbd0f
Asmir Mustafic <goetas@lignano.it>
2012-01-13 12:15:19 +0100

    typo

--- lib/redmine_ldap_sync/redmine_ext/auth_source_ldap_patch.rb
+++ lib/redmine_ldap_sync/redmine_ext/auth_source_ldap_patch.rb
@@ -94,7 +94,7 @@ module RedmineLdapSync
                             :return_result => false) do |entry|
               if attr_enabled=='userAccountControl' && entry[attr_enabled] && entry[attr_enabled][0].to_i & 2 != 0
                 users[:disabled] << entry[self.attr_login][0]
-              elsif attr_enabled.index('samba')=!nil && entry[attr_enabled] && entry[attr_enabled][0].index('D') != nil
+              elsif attr_enabled.index('samba') != nil && entry[attr_enabled] && entry[attr_enabled][0].index('D') != nil
                 users[:disabled] << entry[self.attr_login][0]
               else
                 users[:enabled] << entry[self.attr_login][0]
